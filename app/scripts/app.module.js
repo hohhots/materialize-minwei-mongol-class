e5a67cbaa451aaf770a538d3fbbbbaa4d7e31dc8
brgd <hohhots@gmail.com>
2017-04-21 23:52:54 +0800

    change parallax to header

--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -16,8 +16,8 @@ angular.module('learnMongolApp', [
 (function($){
   $(window).on('load', function() {
 
-    $('.parallax').parallax();
-    $('.dropdown-button').dropdown('close');
+    //$('.parallax').parallax();
+    //$('.dropdown-button').dropdown();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
