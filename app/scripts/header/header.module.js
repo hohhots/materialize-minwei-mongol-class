a5943a90c364b3bb88dd3894405736ff9a84d9aa
brgd <hohhots@gmail.com>
2017-04-22 18:43:42 +0800

    add main dropdown

--- app/scripts/header/header.module.js
+++ app/scripts/header/header.module.js
@@ -2,3 +2,15 @@
 
 // Define the `header` module
 angular.module('header', []);
+
+// For header functions
+(function($){
+  $(window).on('load', function() {
+
+    $("nav #mainDropDown").on( "click", function( event ) {
+      event.preventDefault();
+      console.log( $( this ).text() );
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
