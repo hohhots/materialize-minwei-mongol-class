cfe198951840a64b7d6ec1892f23098f72f6e0bc
brgd <hohhots@gmail.com>
2017-06-08 19:56:36 +0800

    add subject page left panel

--- app/scripts/subject/subject.component.js
+++ app/scripts/subject/subject.component.js
@@ -9,15 +9,43 @@
     .module('subject')
     .component('appSubject', {
       templateUrl: 'scripts/subject/subject.template.html',
-      controller: controller
+      controller: [
+        '$scope',
+        '$element',
+        'Config',
+        'Util',
+        'Json',
+        controller]
     });
 
   function template() {
     return 'scripts/subject/subject.template.html';
   }
 
-  function controller() {
+  function controller($scope, $element, config, util, json) {
 
+    var self = this,
+        beginStickyElemId = "categoryBeginSticky",
+        elem = $element.find("#" + beginStickyElemId),
+        path = util.getUrlPath().substring(1),
+        rootPath = config.json.rootPath;
+console.log(path);
+    var init = function() {
+      //self.category = json.getCategory(path);
+
+    };
+
+    self.jsons = json;
+    self.category = {};
+    self.subjects = {};
+    self.subjectsStyle = {};
+    self.classes = {};
+    self.classesStyle = {};
+    self.ListItemLinkStyle = {};
+    self.subjectItemStyle = {};
+    self.headerStickyHide = true;
+
+    $scope.$watch(function(){return self.jsons;}, init, true);
   }
 
 })(jQuery, window.angular)
