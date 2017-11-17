9437bf84b8bb02176b7b61ff1ccf11e16f6313c9
brgd <hohhots@qq.com>
2017-11-17 15:14:26 +0800

    set mon word height and width

--- app/scripts/word/word.component.js
+++ app/scripts/word/word.component.js
@@ -6,7 +6,7 @@
 
   // Register `headerList` component, along with its associated controller and template
   app.component('monWord', {
-    template: '<div ng-include="$ctrl.templateUrl"></div>',
+    template: '<div ng-include="$ctrl.templateUrl" class="word-outerContainer"></div>',
     bindings: {
       orgintext: '@'
     },
@@ -26,21 +26,37 @@
 
     //define self variables
     self.templateUrl = config.template;
+    self.monText = '';
+    self.containerStyle = {};
 
     self.$postLink = function () {
-      self.containerStyle.position = "absolute";
-      self.containerStyle.width = parentElem.height();
-      self.containerStyle.height = parentElem.width();
+
+      var ow = 0;
+      var oh = 0;
+
+      var dim = $interval(function () {
+        ow = parentElem[0].offsetWidth;
+        oh = parentElem[0].offsetHeight;
+
+        self.containerStyle.position = "absolute";
+        self.containerStyle.width = oh + 'px';
+        self.containerStyle.height = ow + 'px';
+
+        $interval.cancel(dim);
+      }, 20);
+
+
+
+      console.log(parentElem);
+
+      if (parentElem[0].offsetHeight) {
+        oh = parentElem[0].offsetHeight;
+
+      }
 
       self.monText = $sce.trustAsHtml(config.setMonWord(self.orgintext));
-      //setMonText(self.orgintext);
     };
 
-    self.containerStyle = {};
-    self.monText = '';
-
-    var parentElemClass = ".wordbegin-word";
-    var parentElem = $element.parents(parentElemClass);
-
+    var parentElem = $element.parent();
   };
 })();
