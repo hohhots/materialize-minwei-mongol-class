c50dead2e1234f76a2b56c6002794308785c00db
brgd <hohhots@qq.com>
2017-11-29 01:27:23 +0800

    add word play animation

--- app/scripts/player/wordPlayer/wordPlayer.component.js
+++ app/scripts/player/wordPlayer/wordPlayer.component.js
@@ -53,16 +53,19 @@
 
     // define local variables
     var audioElem = null;
+    var seperateHeight = 20;
     var playWord = [];
     var playWordSpans = [];
     var wordParentSpan = '';
+    var playingIndex = 0;
 
     var playerEnded = function () {
-      self.showPlayer = false;
+      self.closePlayer();
       $scope.$digest();
     };
 
     var playWordAnimation = function (event, word) {
+      $element.css('visibility', 'hidden');
       self.showWordPlayer = true;
       self.word = word.word;
       //broadcast after word rendring completed, so time must set largger than word rendring.
@@ -70,48 +73,77 @@
         $scope.$broadcast(config.events.wordGetWordSpans);
         $interval.cancel(dd);
       }, 30);
-      //self.mediasUrl = mediasUrl;
-      //console.log(self.word);
-      //audioElem.load();
-      //audioElem.play();
+
     };
 
     var setWordAnimationElement = function (event, words) {
       playWord = words[0];
       playWordSpans = words[1];
       wordParentSpan = words[2];
-      wordSeperateAnimation();
+      setWordSeperate();
+      wordAnimation();
     };
 
-    var wordSeperateAnimation = function () {
-      console.log();
-
+    var setWordSeperate = function () {
       var parentWidth = wordParentSpan.css('width');
       parentWidth = util.getNumOfDim(parentWidth);
-      
+
       var wordWidth = playWordSpans.css('height');
       wordWidth = util.getNumOfDim(wordWidth);
-      
-      var middleLeft = (parentWidth - wordWidth)/2;
+
+      var middleLeft = (parentWidth - wordWidth) / 2;
 
       $.each(playWordSpans, function (index, val) {
-        //$(val).css('visibility', 'hidden');
-        $(val).css('position', 'absolute');
-        $(val).css('left', '0');
-        $(val).css('top', middleLeft + 'px');
+        $(val).css({
+          'visibility': 'hidden',
+          'position': 'absolute',
+          'left': '0',
+          'top': middleLeft + 'px'
+        });
         if (index) {
           var preSpan = playWordSpans[index - 1];
+
           var top = $(preSpan).css('left');
           top = util.getNumOfDim(top);// + playWordSpans[index - 1].offsetHeight;
 
           var height = $(preSpan).css('width');
           height = util.getNumOfDim(height);
-          top = parseInt(top) + parseInt(height);
-          console.log(top);
+          top = parseInt(top) + parseInt(height) + seperateHeight;
 
           $(val).css('left', top);
         }
       });
+    };
+
+    var wordAnimation = function () {
+      if (playingIndex == playWord.length) {
+        playingIndex = 0;
+        return;
+      }
+      $element.css('visibility', 'visible');
+
+      var animationDone = false;
+      var audioDone = false;
+      var span = $(playWordSpans[playingIndex]);
+      span.css({ 'visibility': 'visible', 'opacity': 0 });
+      span.animate({ "opacity": "+=1" }, 2000, function () {
+        animationDone = true;
+        nextAnimation();
+      });
+
+      audioDone = true;
+
+      var nextAnimation = function () {
+        if (animationDone && audioDone) {
+          ++playingIndex;
+          $interval.cancel(check);
+          wordAnimation();
+        }
+      };
+      //self.mediasUrl = mediasUrl;
+      //console.log(self.word);
+      //audioElem.load();
+      //audioElem.play();
     };
 
     // add listener and hold on to deregister function
